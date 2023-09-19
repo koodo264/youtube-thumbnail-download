4ddca839a3b32b3b76c77c057c2fdf53d110729a
koodo264 <143145557+koodo264@users.noreply.github.com>
2023-09-20 00:45:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Index = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-<header className="text-center mb-8" style={{ width: "42%" }}>
+<header className="text-center mb-8" style={{ width: "65%" }}>
         <h1 className="text-3xl font-bold mb-2">
           Youtube Thumbnail Downloader
         </h1>
